eaaa2df9f976c5f30680e8026792cc1dcec4c8f8
Socha17 <mattsocha11@gmail.com>
2017-04-27 21:18:50 +0000

    styled login

--- public/scripts/homeApp.js
+++ public/scripts/homeApp.js
@@ -1,19 +1,32 @@
 
 
+
+
 $(document).ready(function() {
   $('.submitBtn').on('click', function (event) {
     event.preventDefault();
-    console.log("prevent");
-    console.log($('#email').val());
-    console.log($('#pwd').val());
-    $.ajax({
-      url: '/home',
-      method: 'POST',
-      data: {email: $('#email').val(), password: $('#pwd').val()},
-      success: function () {
-        console.log("success");
-      }
-    });
+
+    let email = $('#email').val();
+    let password = $('#pwd').val();
+
+    console.log(email);
+    console.log(password);
+
+    if (email == "" || email == undefined) {
+      $('#email').addClass( "formError" );
+      $('#pwd').addClass( "formError" );
+      $(".alertMessage").css("display", "block" );
+      setTimeout( function() { $('#email').removeClass( "formError" ); $('#pwd').removeClass( "formError" ); $(".alertMessage").css("display", "none"); }, 4000);
+    } else {
+      $.ajax({
+        url: '/home',
+        method: 'POST',
+        data: {email: email, password: password},
+        success: function () {
+          console.log("success");
+        }
+      });
+    }
   })
 
 });
