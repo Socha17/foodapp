c178917cf4c224a3fcc62813f64c90c5813636d9
BrandonGA <brandon.galeaandrews@gmail.com>
2017-04-27 21:34:48 +0000

    Styling FoodMenu

--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -2,10 +2,29 @@
 const express = require('express');
 const router  = express.Router();
 
+// function to verify if user is logged in
+function verifyUserIsLoggedIn (user) {
+  if (user === undefined) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+const menuDB = {
+  var Pizza {
+    id: 1,
+    unit_price: 5,
+    name: 'pizza',
+    description: 'Ballin',
+    image_id: '/hidden',
+  }
+};
+
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
-    console.log('boobies');
+
     res.render('foodMenu')
   });
 
